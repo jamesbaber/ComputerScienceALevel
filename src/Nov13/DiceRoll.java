6da46e0425779d14e3a192b8fc40fed8b40e3892
Unknown <admin@jamesbaber.co.uk>
2016-11-14 09:03:15 +0000

    Snakeeyesstuff

--- src/Nov13/DiceRoll.java
+++ src/Nov13/DiceRoll.java
@@ -2,25 +2,40 @@ package Nov13;
 
 public class DiceRoll {
 	public static void main(String args[]) {
+		rollToSnakeEyes();
+	}
+	
+	// Snake eyes routine
+	public static void rollToSnakeEyes() {
+		int rollsToSnakeEyes = 0;
+		rollsToSnakeEyes = rollToTarget(2);
+		
+		System.out.println("To get snake eyes it took " + rollsToSnakeEyes + " rolls");
 		
 	}
 	
-	public static int rollTotarget(int target) {
+	// Get number of rolls until total is target
+	public static int rollToTarget(int target) {
+		// Initialise rolls as 0
 		int rolls = 0;
 		
-		if (!(target > 2 && target <= 12)) {
-			throw new IllegalArgumentException("Impossible");
+		// Are the parameters within range?
+		if (!(target >= 2 && target <= 12)) {
+			throw new IllegalArgumentException("Impossible target " + target);
 		}
 		
-		int die1 = -1;
-		int die2 = -2;
+		// Set dice to 0
+		int die1 = 0;
+		int die2 = 0;
 		
-		while (die1 != die2) {
+		// While total not target, keep rolling
+		while ((die1 + die2) != target) {
 			die1 = rollDie();
 			die2 = rollDie();
 			rolls += 1;
 		}
 		
+		// Return the number of rolls it took
 		return(rolls);
 	}
 	
