3f6a21e848ec531e64904ce10e1757cfdd21bf23
Unknown <admin@jamesbaber.co.uk>
2017-02-06 09:34:01 +0000

    Half Passby

--- src/Feb5/PoorDogTestDrive.java
+++ src/Feb5/PoorDogTestDrive.java
@@ -3,16 +3,28 @@ package Feb5;
 public class PoorDogTestDrive {
     public static void main(String[] args)
     {
-        PoorDog one = new PoorDog();
-        System.out.println("Dog size is " + one.getSize());
+        PoorDog one = new PoorDog(); // Make a new god class instance
+        
+        // Print the output of two "getter" methods.
+        System.out.println("Dog size is " + one.getSize()); // 
         System.out.println("Dog name is " + one.getName());
     }
 }
 
 class PoorDog
 {
+	// Private variables. Only accessible from methods of this class.
     private int size;
     private String name;
-    public int getSize() { return size;}
-    public String getName() { return name;}
+    
+    // "Getter" Returns value of private variable.
+    // This stops the calling code from changing the private variable.
+    public int getSize() {
+    	return size;
+    }
+    
+    // Another getter.
+    public String getName() {
+    	return name;
+    }
 }
