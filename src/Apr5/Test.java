73bbfa885c47307b3ee9d8821415e9634c5f9dcc
Unknown <admin@jamesbaber.co.uk>
2017-04-05 12:00:31 +0100

    Mehhh

---
+++ src/Apr5/Test.java
@@ -0,0 +1,16 @@
+package Apr5;
+
+public class Test {
+	public Test() {
+		System.out.println("Test");
+	}
+	public void Useless() {
+		System.out.println("Eh");
+	}
+	
+	
+	public static void main(String args[]) {
+		Test obj = new Test();
+		obj.Useless();
+	}
+}
